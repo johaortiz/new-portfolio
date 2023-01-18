9e9fa73ee6af2d82650757e355334d079dd4399a
johaortiz <surf234@outlook.es>
2023-01-18 04:53:15 -0300

    hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,12 @@
-import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
-import { Box, createTheme, ThemeProvider } from "@mui/material";
-import { useSelector } from "react-redux";
+import { lazy, Suspense } from "react";
 const Drawer = lazy(() => import("./components/Drawer/"));
-import SideBar from "./components/SideBar/SideBar";
-import ButtonTheme from "./components/ButtonTheme/ButtonTheme";
-import Home from "./pages/Home/Home";
-import About from './pages/About/About'
-import Experiencie from "./pages/Experience/Experiencie";
-import Contact from "./pages/Contact/Contact";
-
-const obj = {
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  height: "100vh",
-  bgcolor: "background.default",
-  color: "text.primary",
-};
+import Home from "./pages/Home";
+import About from './pages/About'
+import Experiencie from "./pages/Experience";
+import Contact from "./pages/Contact";
 
 function App() {
-
-  const contextTheme = useSelector((state) => state.theme.theme);
-  const theme = createTheme({
-    palette: {
-      mode: contextTheme,
-    },
-  });
 
   return (
     <div>
@@ -42,25 +22,3 @@ function App() {
 
 export default App;
 
-
-{/* <ThemeProvider theme={theme}>
-      <>
-        <SideBar contextTheme={contextTheme} />
-        <Box
-          component="main"
-          sx={{
-            ...obj,
-            width: "75vw",
-          }}
-        >
-          <ButtonTheme />
-          <Routes>
-            <Route exact path='/' element={<Home />} />
-            <Route exact path='/about' element={<About />} />
-            <Route exact path='/experience' element={<Experiencie />} />
-            <Route exact path='/contact' element={<Contact />} />
-            <Route exact path='*' element={<>Not Found</>} />
-          </Routes>
-        </Box>
-      </>
-    </ThemeProvider> */}
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
+import { store } from './redux/store'
 import App from './App'
 import './index.css'
-import { store } from './redux/store'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>,
+createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>
 )
