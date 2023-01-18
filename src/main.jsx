ef90df83805f70ced9bc8b3ce736e379b79b2bf2
johaortiz <surf234@outlook.es>
2023-01-18 06:45:05 -0300

    rm hash router

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { store } from './redux/store'
 import App from './App'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </Provider>
 )
